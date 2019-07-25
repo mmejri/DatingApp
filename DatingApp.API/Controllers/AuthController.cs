555878c8c8f92613831a490df347d4db4e4eed0c
MEJRI Mohamed (EAC-EXT) <Mohamed.MEJRI@ext.ec.europa.eu>
2019-07-25 17:15:04 +0200

    Auth

---
+++ DatingApp.API/Controllers/AuthController.cs
@@ -0,0 +1,33 @@
+using System.Threading.Tasks;
+using DatingApp.API.Data;
+using DatingApp.API.Dtos;
+using DatingApp.API.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DatingApp.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly IAuthRepository _repo;
+        public AuthController(IAuthRepository repo)
+        {
+            _repo = repo;
+        }
+
+        [HttpPost("Register")]
+        public async Task<IActionResult> Register(UserForRegisterDto userForRegisterDto)
+        {
+            userForRegisterDto.UserName = userForRegisterDto.UserName.ToLower();
+            if (await _repo.UserExists(userForRegisterDto.UserName))
+                return BadRequest("user already exists");
+            var userToCreate = new User
+            {
+                Username = userForRegisterDto.UserName
+            };
+            var createdUser = _repo.Register(userToCreate, userForRegisterDto.Password);
+            return StatusCode(201);
+        }
+    }
+}
